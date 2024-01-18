0edc6cb32ad95c3b5c02a34fb3dd775533095a8b
n-andersen <n.andersen@f5.com>
2024-01-18 09:20:00 +0100

    Code Updates - Thu Jan 18 09:20:00 CET 2024

--- commit.sh
+++ commit.sh
@@ -1,6 +1,3 @@
-git config --global user.email "n.andersen@f5.com"
-git config --global user.name "n-andersen"
-git config --global push.default simple
 git add -A
 git commit -m "Code Updates - $(date)"
 git push 
